518ec2c16197645accea060c9040d25d5c0ebd3b
comp1ler1 <ilya.kovaliov@gmail.com>
2024-05-31 15:21:47 +0300

    XOR is ready and methods for copying files

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,243 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->lineEdit_3->setValidator( new QIntValidator);
+    //ui->lineEdit_4->setValidator( new QIntValidator );
+    ui->lineEdit_4->setInputMask("hhhhhhhhhhhhhhhh");
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+std::wstring MainWindow::getCounterName(std::wstring input){
+
+    std::wregex rgx(L"^.*([(][0-9]*[)][.])");
+    //std::wregex rgx2(L"[(][0-9]*[)][.]");
+    //std::wregex rgx3(L"^.*([.*][(][0-9]*[)])");
+    std::wregex rgx4(L"^.*([.].*)");
+
+    std::wcmatch temp;
+    std::regex_search(input.c_str(), temp, rgx);
+    std::wstring output;
+    if(!temp.empty()){
+        std::wcout << temp[temp.size() - 1] << std::endl;
+
+        auto number = stoi(temp[temp.size() - 1].str().substr(1, temp[temp.size() - 1].str().size() - 3));
+
+        std::wcout << number << std::endl;
+
+        auto numberPrev = number;
+        number += 1;
+        output = input;
+
+        std::wstring wnumber = std::to_wstring(numberPrev);
+        wnumber.insert(0, L"(");
+        wnumber.append(L").");
+
+        std::wstring outputResult = std::regex_replace(output, rgx, wnumber);
+
+        auto inputLen = outputResult.length();
+
+        wnumber = std::to_wstring(number);
+        wnumber.insert(0, L"(");
+        wnumber.append(L").");
+
+        outputResult = std::regex_replace(output, rgx, wnumber);
+
+
+        output.erase(output.size() - inputLen, output.size() - 1);
+        output.append(outputResult);
+
+        std::wcout << output << std::endl;
+    }else{
+        std::regex_search(input.c_str(), temp, rgx4);
+        std::wcout << temp[temp.size() - 1] << std::endl;
+        output = input;
+        output.insert(output.length() - temp[temp.size() - 1].length(), L"(1)");
+        std::wcout << output << std::endl;
+    }
+    return output;
+}
+
+
+std::vector<std::wstring> MainWindow::getFiles(std::wstring input_mask){
+
+    std::vector<std::wstring> files;
+
+    WIN32_FIND_DATAW findFileData;
+    HANDLE hFind = FindFirstFileW(input_mask.c_str(), &findFileData);
+
+    if (hFind != INVALID_HANDLE_VALUE) {
+        do {
+            if (findFileData.dwFileAttributes != FILE_ATTRIBUTE_DIRECTORY) {
+                //std::wcout << findFileData.cAlternateFileName << std::endl;
+                files.push_back(findFileData.cFileName);
+            }
+        } while (FindNextFile(hFind, &findFileData) != 0);
+        FindClose(hFind);
+    }
+
+    return files;
+}
+
+
+void MainWindow::cipherFiles(){
+
+    std::vector<std::wstring> files = getFiles(mask);
+
+    for(size_t i = 0; i < files.size(); i++){
+        //std::ifstream in(files[i].c_str());
+
+        HANDLE fh;
+        fh = CreateFileW( files[i].c_str(), GENERIC_WRITE, 0, NULL,
+                         CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+
+        if (ERROR_SHARING_VIOLATION != GetLastError())
+        {
+           //std::wcout << L"File " << files[i] << L" is not open" << std::endl;
+
+           CloseHandle(fh);
+
+           union buf a;
+
+           std::wstring out_file = files[i];
+           std::ifstream in;
+           std::ofstream out;
+
+           std::wstring tempString = output_path;
+           if(ui->comboBox->currentIndex() == 1){
+
+               tempString.append(files[i]);
+               std::wcout << tempString << std::endl;
+
+               while(1){
+                   std::vector<std::wstring> temp_vector = getFiles(tempString);
+                   std::wstring tempPath = output_path;
+
+                   if(temp_vector.empty()){
+                        break;
+                       //std::wcout << tempPath << std::endl;
+
+                       //std::filesystem::copy_file(files[i].c_str(), tempString.c_str());
+                   }else{
+                       //tempPath.append(files[i]);
+                       std::wstring newFile = getCounterName(temp_vector[temp_vector.size() - 1]);
+
+                       tempPath.append(newFile);
+                       tempString = tempPath;
+                       std::cout << "Oops" << std::endl;
+                   }
+               }
+               std::wcout << tempString << std::endl;
+               //std::filesystem::CopyFileW(files[i], tempString);
+           }
+
+           in.open(files[i].c_str(), std::ios::binary);
+           out.open(tempString.c_str(), std::ios::binary);
+
+           while(!in.eof()){
+                //in.get(a.array, n);
+               int j = 0;
+               for (;j < 8 && in.tellg() != -1; ++j){
+                    in.read(a.array + j, 1);
+                    a.array[j] ^= scalar.array[7 - j];
+               }
+                //in.read(a.array, n);
+                //std::cout << in.tellg() << std::endl;
+                //if(in.tellg() < 0){
+                //    count = in.tellg() + n;
+                //}else{
+                //count = in.tellg() - count;
+                //}
+               //a.number ^= scalar;
+
+                //if(j == 8)
+                //    out.write(a.array, j);
+                //else
+                //    out.write(a.array, j - 1);
+
+                if(!in.eof())
+                    out.write(a.array, j);
+                else
+                    out.write(a.array, j - 1);
+           }
+
+           in.close();
+           out.close();
+
+        }
+        else{
+            //std::cout << GetLastError() << std::endl;
+
+            //std::cout << "File ";
+            //std::wcout << files[i];
+            //std::cout << " was opened by other application" << std::endl;
+        }
+    }
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    std::cout << "Button is clicked" << std::endl;
+
+    if(output_path.empty()){
+        output_path = std::filesystem::current_path();
+        //std::wcout << output_path << std::endl;
+        output_path.append(L"\\\\");
+    }
+
+    //std::filesystem::copy_file("test.txt", "test1.txt");
+
+    //std::wcout << std::filesystem::current_path() << std::endl;
+    //std::wcout << output_path << std::endl;
+
+    cipherFiles();
+}
+
+
+void MainWindow::on_lineEdit_textChanged(const QString &arg1)
+{
+    std::cout << arg1.toStdString() << std::endl;
+    mask = arg1.toStdWString();
+}
+
+
+void MainWindow::on_lineEdit_5_textChanged(const QString &arg1)
+{
+    std::cout << arg1.toStdString() << std::endl;
+    output_path = arg1.toStdWString();
+}
+
+
+void MainWindow::on_lineEdit_4_textChanged(const QString &arg1)
+{
+    //bool flag;
+    //scalar = arg1.toUInt(&flag, 10);
+    scalar.number = arg1.toULongLong(&flag, 16);
+    std::cout << arg1.toStdString() << std::endl;
+    std::cout << scalar.number << std::endl;
+    //for(int i = 0; i < 8; i++){
+    //    std::cout << scalar.array[i] << std::endl;
+    //}
+}
+
+
+void MainWindow::on_lineEdit_3_textChanged(const QString &arg1)
+{
+    //bool flag;
+    time_in_seconds = arg1.toUInt(&flag, 10);
+    std::cout << time_in_seconds << std::endl;
+}
+
+
+void MainWindow::on_comboBox_currentIndexChanged(int index)
+{
+    std::cout << "Current index: " << index << std::endl;
+    std::cout << ui->comboBox->currentIndex() << std::endl;
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,19 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <iostream>
+#include <string>
+#include <filesystem>
+#include <windows.h>
+#include <vector>
+#include <fstream>
+#include <regex>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+union buf{unsigned long long number; char array[8];};
 
 class MainWindow : public QMainWindow
 {
@@ -15,7 +24,38 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+    //void updateTime();
+
+    void on_lineEdit_textChanged(const QString &arg1);
+
+    //void on_lineEdit_2_textChanged(const QString &arg1);
+
+    //void on_lineEdit_5_textChanged(const QString &arg1);
+
+    void on_lineEdit_2_textChanged(const QString &arg1);
+
+    void on_lineEdit_5_textChanged(const QString &arg1);
+
+    void on_lineEdit_4_textChanged(const QString &arg1);
+
+    void on_lineEdit_3_textChanged(const QString &arg1);
+
+    void on_comboBox_currentIndexChanged(int index);
+
 private:
     Ui::MainWindow *ui;
+    bool flag;
+
+    unsigned long long time_in_seconds;
+    union buf scalar;
+    std::wstring mask, output_path;
+
+    void cipherFiles();
+    std::vector<std::wstring> getFiles(std::wstring input_mask);
+
+    std::wstring getCounterName(std::wstring input);
+
 };
 #endif // MAINWINDOW_H
